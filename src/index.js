31728829493c6c489273a1be277588bc289e7e12
Krystle Justine <KrystleJustine@gmail.com>
2019-03-18 12:45:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const formTag = document.querySelector('#create-task-form');
+const ulTag = document.querySelector('#tasks');
+const deleteTag = document.querySelector('#')
+const addTask = (task) => {
+    liTag = `<li>${task}</li>`;
+    ulTag.innerHTML+= liTag
+};
+
+formTag.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    let task = e.target.parentElement.querySelector('#new-task-description').value;
+    addTask(task);
 });
+
+// need a function to delete a specific task
+const deleteTask = (task) => {
+
+};
